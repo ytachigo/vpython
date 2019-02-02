11a924da47f0ffe4920a2192990b2427e367f25b
ytachigo <tachi.yuhei@gmail.com>
2019-02-03 03:09:13 +0900

    fixed

---
+++ sin-lfrog.py
@@ -0,0 +1,37 @@
+from vpython import *
+
+def func1(x, v, t): # Velocity
+    return v
+
+def func2(x, v, t): # Potential energy function
+    return -sin(x)
+
+def lfrog(x, v, t, h): # Leap frog method
+    next_x = x + func1(x,v,t) * h
+    next_v = v + func2(x,v,t) * h
+    return [next_x,next_v]
+
+i = 0 # Set up initial values
+h = 0.01
+phi = 30
+x0 = phi / 180.0 * pi
+v0 = 0.0
+
+x = -func2(x0,v0,0) # Set objects
+y = 0.5 - cos(x0)
+ball = sphere(pos=vector(x,y,0),color=color.red, radius=0.15)
+cyl = cylinder(pos=ball.pos, axis=-ball.pos,
+               color=color.white, radius=0.01)
+
+while 1: # Iteration
+    rate(400)
+    i += 1
+    time = i * h
+    next = lfrog(x0,v0,time,h)
+    x0 = next[0]
+    v0 = next[1]
+    x = -func2(x0,v0,0)
+    y = 0.5 - cos(x0)
+    ball.pos = vector(x,y,0)
+    cyl.pos = ball.pos
+    cyl.axis = -ball.pos
--- sin-runge.py
+++ sin-runge.py
@@ -1,6 +1,6 @@
 from vpython import *
 
-def func1(x, v, t): # Velosity
+def func1(x, v, t): # Velocity
     return v
 
 def func2(x, v, t): # Potential energy function
@@ -15,31 +15,31 @@ def runge4(x, v, t, h): # 4th Order Runge–Kutta method
     m3 = h * func2(x + k2 / 2,v + m2 / 2,t + h / 2)
     k4 = h * func1(x + k3,v + m3,t + h)
     m4 = h * func2(x + k3,v + m3,t + h)
-    delata_x = (k1 + k4 + 2 * (k2 + k3)) / 6.0
-    delata_v = (m1 + m4 + 2 * (m2 + m3)) / 6.0
-    return [delata_x, delata_v]
+    delta_x = (k1 + k4 + 2 * (k2 + k3)) / 6.0
+    delta_v = (m1 + m4 + 2 * (m2 + m3)) / 6.0
+    return [delta_x,delta_v]
 
-h = 0.01 # Set up initial values
+i = 0 # Set up initial values
+h = 0.01
 phi = 30
 x0 = phi / 180.0 * pi
 v0 = 0.0
-i = 0
-x = sin(x0)
-y = 0.5 - cos(x0)
 
-ball = sphere(pos=vector(x,y,0),color=color.red, radius=0.15) # Set objects
+x = -func2(x0,v0,0) # Set objects
+y = 0.5 - cos(x0)
+ball = sphere(pos=vector(x,y,0),color=color.red, radius=0.15)
 cyl = cylinder(pos=ball.pos, axis=-ball.pos,
                color=color.white, radius=0.01)
 
 while 1: # Iteration
     rate(400)
+    i += 1
     time = i * h
     delta = runge4(x0,v0,time,h)
     x0 += delta[0]
     v0 += delta[1]
-    x = sin(x0)
+    x = -func2(x0,v0,0)
     y = 0.5 - cos(x0)
     ball.pos = vector(x,y,0)
     cyl.pos = ball.pos
     cyl.axis = -ball.pos
-    i += 1
